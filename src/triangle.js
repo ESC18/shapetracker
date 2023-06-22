1c25fd430a41766a1ad6ce674420745c7238d263
ESC18 <ponderosaprintingus@gmail.com>
2023-06-22 10:41:25 -0700

    Third test passed - now determines if triangle is isosceles.

--- src/triangle.js
+++ src/triangle.js
@@ -6,6 +6,13 @@ export default function Triangle(side1, side2, side3, height) {
     this.isScalene = ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) ? "is scalene" : "not scalene";
     this.isEquilateral = ((this.side1 === this.side2) && (this.side1 === this.side3)) ? "is equilateral" : "not equilateral";
     Triangle.prototype.checkType = function() {
-        return "not a triangle";
-    }
+        if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
+          return "not a triangle";
+        } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
+          return "scalene triangle";
+        }  else if ((this.side1 === this.side2) || ((this.side1 === this.side3)) || ((this.side2 === this.side3))) {
+            return "isosceles triangle";
+          }
+      };
+
 }
